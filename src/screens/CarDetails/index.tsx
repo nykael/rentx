27417cb35fcc9397963a728afe6705f0068ac968
nykael <nykaelccardial@gmail.com>
2022-04-19 11:28:42 -0300

    component slid

--- src/screens/CarDetails/index.tsx
+++ src/screens/CarDetails/index.tsx
@@ -1,9 +1,11 @@
 import React from "react";
 import { BackButton } from "../../components/BackButton";
+import { ImageSlider } from "../../components/imageSlider";
 
 import {
 Container,
 Header,
+CardImages
 } from './styles'
 
 export function CarDetails () {
@@ -12,6 +14,12 @@ export function CarDetails () {
       <Header>
         <BackButton onPress={() => {}} />
       </Header>
+
+      <CardImages>
+        <ImageSlider 
+          imagesUrl={['https://www.carliveryauto.com/wp-content/uploads/2020/01/1845987-audi-audi-png-2100_1386.png']}
+        />
+      </CardImages>
     </Container>
   )
 }
