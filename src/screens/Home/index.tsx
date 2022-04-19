435281c42b95f87aae87af7f45446567ddc96834
nykael <nykaelccardial@gmail.com>
2022-04-19 09:19:38 -0300

    FlatList

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -4,7 +4,10 @@ import {RFValue} from 'react-native-responsive-fontsize'
 
 import Logo from '../../assets/logo.svg'
 
+import { Car } from "../../components/car";
+
 import { 
+  CarList,
   Container, 
   Header,
   HeaderContent,
@@ -12,6 +15,15 @@ import {
 } from "./styles";
 
 export function Home () {
+  const carData = {
+  brand: 'audi',
+  name: 'RS 5 coupé',
+  rent: {
+    period:'AO DIA',
+    price: 120,
+  },
+  thumbnail: 'https://www.carliveryauto.com/wp-content/uploads/2020/01/1845987-audi-audi-png-2100_1386.png'
+  }
   return(
     <Container>
       <Header>
@@ -30,6 +42,11 @@ export function Home () {
           </TotalCars>
        </HeaderContent>
       </Header>
+      <CarList 
+        data={[1,2,3,4,5,6,7]}
+        keyExtractor={item => String(item)}
+        renderItem={({item}) => <Car data={carData}/>}
+      />
     </Container>
   )
 }
