e8aaf2deae8bcc23263070f32b818698429227b5
nykael <nykaelccardial@gmail.com>
2022-04-19 16:20:52 -0300

    details interface completed

--- src/screens/CarDetails/index.tsx
+++ src/screens/CarDetails/index.tsx
@@ -1,12 +1,33 @@
 import React from "react";
 import { BackButton } from "../../components/BackButton";
 import { ImageSlider } from "../../components/imageSlider";
+import { Accessory } from "../../components/Accessory";
+import { Button } from "../../components/Button";
+
+import speedSvg from '../../assets/speed.svg'
+import accelerationSvg from '../../assets/acceleration.svg'
+import forceSvg from '../../assets/force.svg'
+import gasolineSvg from '../../assets/gasoline.svg'
+import exchangeSvg from '../../assets/exchange.svg'
+import peopleSvg from '../../assets/people.svg'
 
 import {
 Container,
 Header,
-CardImages
+CardImages,
+Content,
+Details,
+Description,
+Brande,
+Name,
+Rent,
+Period,
+Price,
+About,
+Accessories,
+Footer,
 } from './styles'
+import theme from "../../styles/theme";
 
 export function CarDetails () {
   return(
@@ -20,6 +41,48 @@ export function CarDetails () {
           imagesUrl={['https://www.carliveryauto.com/wp-content/uploads/2020/01/1845987-audi-audi-png-2100_1386.png']}
         />
       </CardImages>
+
+      <Content>
+        <Details>
+          <Description>
+            <Brande>Lamborghini</Brande>
+            <Name>Huracan</Name>
+          </Description>
+
+          <Rent>
+            <Period>Ao dia</Period>
+            <Price>R$ 580</Price>
+          </Rent>
+        </Details>
+        <Accessories>
+          <Accessory name="380km/h" icon={speedSvg}/>
+          <Accessory name="3.2s" icon={accelerationSvg}/>
+          <Accessory name="800 HP" icon={forceSvg}/>
+          <Accessory name="Gasolina" icon={gasolineSvg}/>
+          <Accessory name="Auto" icon={exchangeSvg}/>
+          <Accessory name="2 pessoas" icon={peopleSvg}/>
+        </Accessories>
+
+        <About>
+          Este é um automóvel desportivo.  Surgiu do lendário
+          touro de lide indultado na praça Real Maestranza da Sevilla.
+          É um belíssimo carro pra quem gosta de acelerar.
+        </About>
+        <About>
+          Este é um automóvel desportivo.  Surgiu do lendário
+          touro de lide indultado na praça Real Maestranza da Sevilla.
+          É um belíssimo carro pra quem gosta de acelerar.
+        </About>
+        <About>
+          Este é um automóvel desportivo.  Surgiu do lendário
+          touro de lide indultado na praça Real Maestranza da Sevilla.
+          É um belíssimo carro pra quem gosta de acelerar.
+        </About>
+      </Content>
+      
+      <Footer>
+        <Button title="Confirmar"/>
+      </Footer>
     </Container>
   )
 }
--- src/screens/CarDetails/styles.ts
+++ src/screens/CarDetails/styles.ts
@@ -1,4 +1,5 @@
-import { getStatusBarHeight } from "react-native-iphone-x-helper";
+import { getBottomSpace, getStatusBarHeight } from "react-native-iphone-x-helper";
+import { RFValue } from "react-native-responsive-fontsize";
 import styled from "styled-components/native";
 
 export const Container = styled.View`
@@ -18,4 +19,82 @@ export const Header = styled.View`
   position:absolute;
   margin-top: ${getStatusBarHeight() + 18}px;
   margin-left: 24px ;
+`;
+
+export const Content = styled.ScrollView.attrs({
+  contentContainerStyle: {
+    padding:24,
+    alignItems: 'center'
+  },
+  showsVerticalScrollIndicator: false
+})`
+
+`;
+export const Details = styled.View`
+  width:100% ;
+
+  flex-direction:row;
+  align-items:center;
+  justify-content:space-between ;
+
+  margin-top: 28px;
+`;
+export const Description = styled.View`
+
+`;
+export const Brande = styled.Text`
+  font-family:${({theme}) => theme.fonts.secondary_500} ;
+  color:${({theme}) => theme.colors.text_detail};
+  font-size:${RFValue(10)}px ;
+
+  text-transform: uppercase ;
+`;
+export const Name = styled.Text`
+  font-family:${({theme}) => theme.fonts.secondary_500} ;
+  color:${({theme}) => theme.colors.title};
+  font-size:${RFValue(25)}px ;
+`;
+export const Rent = styled.View`
+
+`;
+export const Period = styled.Text`
+  font-family:${({theme}) => theme.fonts.secondary_500} ;
+  color:${({theme}) => theme.colors.text_detail};
+  font-size:${RFValue(10)}px ;
+  text-transform: uppercase ;
+
+`;
+export const Price = styled.Text`
+  font-family:${({theme}) => theme.fonts.secondary_500} ;
+  color:${({theme}) => theme.colors.main};
+  font-size:${RFValue(25)}px ;
+`;
+
+export const About = styled.Text`
+  font-family:${({theme}) => theme.fonts.primary_400} ;
+  color:${({theme}) => theme.colors.text_detail};
+  font-size:${RFValue(15)}px;
+  text-align:justify ;
+
+  margin-top:23px ;
+  line-height:${RFValue(25)}px ;
+`;
+
+export const Accessories = styled.View`
+  width:100%;
+
+  flex-direction:row ;
+  flex-wrap:wrap;
+  align-items:center;
+  justify-content:space-between ;
+  
+  margin-top:16px ;
+  
+`;
+
+export const Footer = styled.View`
+ width:100% ;
+ background-color:${({theme}) => theme.colors.backgorund_secondaty} ;
+ 
+ padding: 24px 24px ${getBottomSpace() + 24}px ;
 `;
